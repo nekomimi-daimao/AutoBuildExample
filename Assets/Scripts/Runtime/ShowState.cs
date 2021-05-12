e43c081ee2124213b386623f1d50442e2b4c3731
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2021-05-12 19:28:10 +0900

    init scene

---
+++ Assets/Scripts/Runtime/ShowState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ShowState : MonoBehaviour
+{
+    private const PrimitiveType Type = PrimitiveType.Cube;
+
+    private void Start()
+    {
+        GameObject.CreatePrimitive(Type);
+    }
+}
